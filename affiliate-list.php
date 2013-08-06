444db89cf44e34ac0fae61d6390d7cf6f371d8e1
wikitopian <matt@swarmstrategies.com>
2013-08-05 18:42:31 -0600

    init

---
+++ affiliate-list.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Affiliate List
+Plugin URI: http://www.github.com/wikitopian/affiliate-list
+Description: Easily add and remove affiliate thumbs from site
+Version: 0.1.0
+Author: Matt Parrott
+Author URI: http://www.swarmstrategies.com/matt
+License: GPLv2
+ */
+
+class Affiliate_List {
+
+    public function __construct() {
+
+    }
+
+}
+
+$affiliate_list = new Affiliate_List();
